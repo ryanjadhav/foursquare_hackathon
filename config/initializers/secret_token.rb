727cc116b771153d81fc43b534372a9154a7008d
Ryan Jadhav <ryan.jadhav@gmail.com>
2013-01-05 12:01:48 -0800

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foursquarehack::Application.config.secret_token = '7543670c3d9f1cfe961c36a561a25c9e126f6386751902a120719bbf5cacd2248864d389c694664befd1d55ef29ccf87e78013b150c451b24a6427b0a05cd1a7'
